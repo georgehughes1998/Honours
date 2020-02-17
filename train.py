6a17b57e278a0ec0ea26d0f4dafdfae22bdbc6cb
georgehughes1998 <ghughes@robotarium.cm.cluster>
2020-02-17 18:24:48 +0000

    Additional changes after training

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ ALLOWED_CHARS = string.ascii_letters + string.digits + string.punctuation + " "
 
 DATASET_FILE_PATHS = ["data/allabcwrepeats_parsed.txt"]
 
-LEARNING_RATE = 5
+LEARNING_RATE = 0.5
 
 BATCH_SIZE = 32
 

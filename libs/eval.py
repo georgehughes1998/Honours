cec520bff29407efed470129d42ccbbb4baa3458
George <hughesgeorgem@gmaill.com>
2020-02-26 13:57:52 +0000

    - Sum the losses since log to get "log-likelihood" - Cutoff before the first pad symbol

--- libs/eval.py
+++ libs/eval.py
@@ -2,25 +2,26 @@ import torch
 import numpy as np
 
 def calculate_perplexity(model, dataset_lines, pad_ix, device=None):
-    # TODO: Fix
+    # TODO: Confirm this implementation is correct
     num_words = 0
-    prod = 1
+    log_likelihood = 0
 
     model.eval()
     with torch.no_grad():
 
         for l in dataset_lines:
 
-            cutoff = [i.item() for i in l].index(pad_ix)
-            num_words += cutoff
+            cutoff = [i.item() for i in l].index(pad_ix) - 1
+            num_words += cutoff + 1
             line = l.to(device)[:cutoff]
 
             res = model(line[:-1])
 
             probs = [res[n, 0, line[n+1].item()].item() for n in range(len(line)-1)]
-            prod = prod * np.product(probs)
+            log_likelihood = log_likelihood + np.sum(probs)
 
-        perp = 2**(-(prod / num_words))
+        # perp = 2**(-(log_likelihood / num_words))
+        perp = np.exp(-(log_likelihood / num_words))
         return perp
 
 

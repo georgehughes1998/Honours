8d0badc5055f3ded6c977107aba432874ac123c3
George <hughesgeorgem@gmaill.com>
2020-02-27 16:47:07 +0000

    - Changed charset parameter to be vocab since working at word level - Added MultiTask network model which tags each word

--- model.py
+++ model.py
@@ -28,20 +28,20 @@ def save_state_dict(state_dict, epoch, batch, loss):
 
 
 class RNN(nn.Module):
-    def __init__(self, charset_size, hidden_size=64, embedding_size=64):
+    def __init__(self, vocab_size, hidden_size=64, embedding_size=64):
 
         super(RNN, self).__init__()
 
         self.hidden_size = hidden_size
         self.embedding_size = embedding_size
 
-        self.char_embeddings = nn.Embedding(charset_size, embedding_size)
+        self.char_embeddings = nn.Embedding(vocab_size, embedding_size)
         self.embeddings_dropout = nn.Dropout(0.3)
 
         self.lstm = nn.LSTM(embedding_size, hidden_size)
         self.lstm_dropout = nn.Dropout(0.5)
 
-        self.decode = nn.Linear(hidden_size, charset_size)
+        self.decode = nn.Linear(hidden_size, vocab_size)
 
         self.softmax = nn.LogSoftmax(dim=-1)
 
@@ -56,14 +56,49 @@ class RNN(nn.Module):
 
         output = self.decode(hidden)
 
-        # section_output = self.section_decode(hidden)
-
         output = self.softmax(output)
-#
-        # section_output = self.softmax(section_output)
+
         return output  # , section_output
 
 
 # For dropout values:
 # -https://medium.com/@bingobee01/a-review-of-dropout-as-applied-to-rnns-72e79ecd5b7b
 # -http://jmlr.org/papers/volume15/srivastava14a.old/srivastava14a.pdf
+
+
+# TODO: Test
+class MultiTaskRNN(nn.Module):
+    def __init__(self, vocab_size, tagset_size, hidden_size=64, embedding_size=64):
+
+        super(MultiTaskRNN, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.embedding_size = embedding_size
+
+        self.char_embeddings = nn.Embedding(vocab_size, embedding_size)
+        self.embeddings_dropout = nn.Dropout(0.3)
+
+        self.lstm = nn.LSTM(embedding_size, hidden_size)
+        self.lstm_dropout = nn.Dropout(0.5)
+
+        self.decode = nn.Linear(hidden_size, vocab_size)
+        self.tagger_decode = nn.Linear(hidden_size, tagset_size)
+
+        self.softmax = nn.LogSoftmax(dim=-1)
+
+    def forward(self, sentence, batch_size=1):
+
+        embeddings = self.char_embeddings(sentence)
+        embeddings = embeddings.view(len(sentence), batch_size, -1)
+        embeddings = self.embeddings_dropout(embeddings)
+
+        hidden, _ = self.lstm(embeddings)
+        hidden = self.lstm_dropout(hidden)
+
+        output = self.decode(hidden)
+        output = self.softmax(output)
+
+        tagger_output = self.tagger_decode(hidden)
+        tagger_output = self.softmax(tagger_output)
+
+        return output, tagger_output

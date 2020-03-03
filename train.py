abc33d2e800e33ff0e2bf57904edb9a21226eddc
George <hughesgeorgem@gmaill.com>
2020-03-03 15:33:43 +0000

    Reset for training with 2 hidden layers

--- train.py
+++ train.py
@@ -80,13 +80,13 @@ dataset_tensors_pairs = [[s[:-1], s[1:]] for s in dataset.get_tensors_data()]
 loader = DataLoader(dataset_tensors_pairs, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
 num_batches = dataset.get_dataset_size() // BATCH_SIZE
 
-# Create an instance of the model
+# Create an instance of the model with given hyperparameters
 rnn = RNN(dataset.vocab_size,
           hidden_size=64,
           embedding_size=64,
           embeddings_dropout=0.3,
           lstm_dropout=0.5,
-          num_decode_layers=5,
+          num_decode_layers=2,
           device=device)
 rnn.to(device)
 

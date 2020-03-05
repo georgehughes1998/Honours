402d55061292834eb7fe1f5b2cf1cbe7e00d44d1
George <hughesgeorgem@gmaill.com>
2020-03-05 13:45:05 +0000

    Send generated files to specific folders depending on their filetype

--- generate.py
+++ generate.py
@@ -8,10 +8,19 @@ from abc2midi import write_abc_file, generate_midi_file, generate_ext_file
 from project_constants import *
 
 
+NUM_SAMPLE_TO_GENERATE = 20
+MAX_GEN_LENGTH = 400
+
+OUTPUT_PATH = "output/"
+ABC_PATH = OUTPUT_PATH + "abc/"
+MIDI_PATH = OUTPUT_PATH + "midi/"
+PNG_PATH = OUTPUT_PATH + "png/"
+
+
 def write_abc(file_name, abc_string, title, do_print=False):
-    write_abc_file(file_name, abc_string, title)
-    generate_midi_file(file_name, file_name, do_print=do_print)
-    generate_ext_file(file_name, file_name, file_extension="png", do_print=do_print)
+    write_abc_file(ABC_PATH + file_name, abc_string, title)
+    generate_midi_file(ABC_PATH + file_name, MIDI_PATH + file_name, do_print=do_print)
+    generate_ext_file(MIDI_PATH + file_name, PNG_PATH + file_name, file_extension="png", do_print=do_print)
 
 
 def remove_symbols(the_string):
@@ -66,126 +75,24 @@ start_symbol = dataset.get_start_symbol()
 end_symbol = dataset.get_end_symbol()
 
 start_prompt_string = start_symbol + " "
-length = 200
 
 
-result = greedy_search(rnn, dataset, start_prompt_string.split(), length)
+result = greedy_search(rnn, dataset, start_prompt_string.split(), MAX_GEN_LENGTH)
 print("Greedy Search with prompt='{}'\n{}".format(start_prompt_string, result))
 print()
-write_abc("output/greedy", result, "greedy")
+write_abc("greedy", result, "greedy")
 
-NUM_SAMPLE_TO_GENERATE = 10
+
 for i in range(NUM_SAMPLE_TO_GENERATE):
     seed = (i+20)*15
     title = str(seed)
 
-    result = random_sample(rnn, dataset, start_prompt_string.split(), length, seed_value=seed)
+    result = random_sample(rnn, dataset, start_prompt_string.split(), MAX_GEN_LENGTH, seed_value=seed)
     result = remove_symbols(result)
 
     print("Sample {}: Random Sample with prompt='{}', seed={}\n{}".format(i, start_prompt_string, seed, result))
 
     try:
-        write_abc("output/sample{}".format(i), result, title, do_print=False)
+        write_abc("sample{}".format(i), result, title, do_print=False)
     except AssertionError:
         print("This ABC string was probably not valid.")
-
-
-# def model_get_probabilities(model, prompt="george"):
-#     output = model(dataset.get_tensor_from_string(prompt))
-#     probabilities = torch.exp(output[-1].view(-1))
-#
-#     probabilities = {dataset.ix_to_vocab[c]: probabilities[c].item() for c in range(dataset.vocab_size)}
-#     return probabilities
-#
-#
-# def model_generate_tree(model, prompt="george", length=5, prob_cutoff_number=2):
-#     if not isinstance(prompt, list):
-#         prompt = prompt.split()
-#     probabilities = model_get_probabilities(model, prompt=prompt)
-#
-#     # Get the second largest probability
-#     probs = [probabilities[c] for c in probabilities]
-#     probs.sort()
-#
-#     new_probabilities = dict()
-#     for i in probs[-prob_cutoff_number-1:]:
-#         for c in probabilities:
-#             if probabilities[c] >= i:
-#                 new_probabilities[c] = probabilities.pop(c)
-#                 break
-#
-#     probabilities = new_probabilities
-#
-#     if length > 1:
-#         leaves = {}
-#         for c in probabilities:
-#             next_probabilities = model_generate_tree(model, prompt=prompt+[c], length=length - 1)
-#             node = (probabilities[c], next_probabilities)
-#             leaves[c] = node
-#         output = leaves
-#     else:
-#         for c in probabilities:
-#             probabilities[c] = (probabilities[c], {})
-#         output = probabilities
-#
-#     return output
-#
-#
-# # Function to return a generated string from the model
-# def generate_text_from_model(model, prompt="george", length=15, max_prompt_length=15):
-#     p = 0
-#     model.eval()
-#     prompt = prompt.split()
-#     for i in range(length):
-#         with torch.no_grad():
-#             input_tensor = dataset.get_tensor_from_string(prompt[p:p+max_prompt_length])
-#             output = model(input_tensor)
-#             # output = output.permute(0, 2, 1)
-#             prompt += [dataset.ix_to_vocab[torch.argmax(output, dim=2)[-1].item()]]
-#             p += 1
-#     model.train()
-#
-#     text_prompt = ""
-#     for w in prompt:
-#         text_prompt += w + ' '
-#
-#     return text_prompt
-#
-#
-# # Test initial model by generating some strings
-# for i in range(1):
-#     print(generate_text_from_model(rnn, prompt="george hughes", length=40))
-#     print(generate_text_from_model(rnn, prompt="zoe hughes", length=40))
-#     print(generate_text_from_model(rnn, prompt="callum davies", length=40))
-# print()
-
-# print(generate_text_from_model(rnn, prompt="<S>", length=40))
-# print()
-
-
-# def get_tree_string(tree, substring, probability=1):
-#     substrings = []
-#
-#     if len(tree) == 0:
-#         return_value = [(substring, probability)]
-#     else:
-#         for c in tree:
-#             substrings += get_tree_string(tree[c][1], substring + ' ' + c, probability*tree[c][0])
-#
-#         return_value = substrings
-#     return return_value
-#
-#
-# def print_tree(tree, substring, do_sort=True):
-#     values = get_tree_string(tree, substring)
-#
-#     if do_sort:
-#         values.sort(key=lambda x: x[1])
-#
-#     for s,p in values:
-#         print(s, "(probability={})".format(p))
-
-# rnn.eval()
-# prompt = "<S>"
-# tree = model_generate_tree(rnn, prompt , length=10, prob_cutoff_number=10)
-# print_tree(tree, substring=prompt, do_sort=True)

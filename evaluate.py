1e281e596179dcf3587e396808a3cd17680af6d3
George <hughesgeorgem@gmaill.com>
2020-03-31 21:55:04 +0100

    Evaluate perplexity of multi task models

--- evaluate.py
+++ evaluate.py
@@ -1,10 +1,13 @@
 import torch
 import sys
 
-from model import RNN, load_state_dict
+from model import RNN, load_state_dict, MultiTaskRNN
+from libs.data_manager_tag import DatasetManagerTag
 from libs.data_manager import DatasetManager, VALIDATION_DATA
 from libs.eval import calculate_perplexity
 from project_constants import *
+
+USE_MULTI_TASK = False
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -12,31 +15,54 @@ print("Device to use:", device)
 print()
 
 
-dataset = DatasetManager(save_path=DATASET_INFO_PATH)
+if not USE_MULTI_TASK:
+    dataset_info_path = DATASET_INFO_PATH
+else:
+    dataset_info_path = DATASET_TAG_INFO_PATH
+
+if not USE_MULTI_TASK:
+    dataset = DatasetManager(save_path=DATASET_INFO_PATH)
+else:
+    dataset = DatasetManagerTag(save_path=DATASET_TAG_INFO_PATH)
 
 try:
     dataset.load()
-    print("Successfully loaded dataset information from {}.".format(DATASET_INFO_PATH))
+    print("Successfully loaded dataset information from {}.".format(dataset_info_path))
 except FileNotFoundError:
-    print("Failed to load dataset information from {}.".format(DATASET_INFO_PATH))
+    print("Failed to load dataset information from {}.".format(dataset_info_path))
     sys.exit(1)
 
 
 # Create an instance of the model with given hyperparameters
-rnn = RNN(dataset.vocab_size,
-          hidden_size=MODEL_HIDDEN_SIZE,
-          embedding_size=MODEL_EMBEDDING_SIZE,
-          embeddings_dropout=MODEL_EMBEDDINGS_DROPOUT,
-          lstm_dropout=MODEL_LSTM_DROPOUT,
-          num_decode_layers=MODEL_NUM_HIDDEN_LAYERS)
+if not USE_MULTI_TASK:
+    # Create an instance of the model with given hyperparameters
+    rnn = RNN(dataset.vocab_size,
+              hidden_size=MODEL_HIDDEN_SIZE,
+              embedding_size=MODEL_EMBEDDING_SIZE,
+              embeddings_dropout=MODEL_EMBEDDINGS_DROPOUT,
+              lstm_dropout=MODEL_LSTM_DROPOUT,
+              num_decode_layers=MODEL_NUM_HIDDEN_LAYERS)
+else:
+    rnn = MultiTaskRNN(dataset.vocab_size,
+                       dataset.tag_vocab_size,
+                       hidden_size=MODEL_HIDDEN_SIZE_MULTI,
+                       embedding_size=MODEL_EMBEDDING_SIZE_MULTI,
+                       embeddings_dropout=MODEL_EMBEDDINGS_DROPOUT_MULTI,
+                       lstm_dropout=MODEL_LSTM_DROPOUT_MULTI,
+                       num_decode_layers=MODEL_NUM_HIDDEN_LAYERS_MULTI)
 rnn.eval()
 
 # Load state dict
 try:
-    state_dict, epoch, batch, best_loss = load_state_dict(device, STATE_DICT_PATH)
+    if USE_MULTI_TASK:
+        state_dict_path = STATE_DICT_PATH_MULTI
+    else:
+        state_dict_path = STATE_DICT_PATH
+
+    state_dict, epoch, batch, best_loss = load_state_dict(device, state_dict_path)
     rnn.load_state_dict(state_dict)
 
-    print("Successfully loaded model state from {}.".format(STATE_DICT_PATH))
+    print("Successfully loaded model state from {}.".format(state_dict_path))
     print("Loaded model at epoch {}, batch {}.".format(epoch, batch))
     print("Best recorded loss was {}.".format(best_loss))
 except FileNotFoundError:
@@ -50,4 +76,8 @@ pad_ix = dataset.get_pad_ix()
 
 print("Calculating perplexity over {} sentences of validation data.".format(len(validation_data)))
 perplexity = calculate_perplexity(rnn, validation_data, pad_ix)
+if USE_MULTI_TASK:
+    perplexity, tag_perplexity = perplexity
 print("Calculated perplexity:", perplexity)
+if USE_MULTI_TASK:
+    print("Calculated perplexity for tags:", tag_perplexity)
--- libs/eval.py
+++ libs/eval.py
@@ -2,36 +2,60 @@ import torch
 import numpy as np
 
 def calculate_perplexity(model, dataset_lines, pad_ix, device=None):
-    # TODO: Confirm this implementation is correct
     num_words = 0
     log_likelihood = 0
+    tags_likelihood = None
+    tags = None
+    res_tags = None
 
     model.eval()
     with torch.no_grad():
 
         for l in dataset_lines:
 
-            cutoff = [i.item() for i in l].index(pad_ix) - 1
-            num_words += cutoff + 1
-            line = l.to(device)[:cutoff]
+            # Multi-task case
+            if isinstance(l, tuple):
+                tune, tags = l
+            else:
+                tune = l
 
+            # Discount padding
+            try:
+                cutoff = [i.item() for i in tune].index(pad_ix) - 1
+            except ValueError:
+                cutoff = len(tune)
+
+            num_words += cutoff + 1
+            line = tune.to(device)[:cutoff]
+
+            # Run the model
             res = model(line[:-1])
 
-            probs = [res[n, 0, line[n+1].item()].item() for n in range(len(line)-1)]
+            # Multi-task case
+            if isinstance(res, tuple):
+                res_tune, res_tags = res
+            else:
+                res_tune = res
+
+            probs = [res_tune[n, 0, line[n+1].item()].item() for n in range(len(line)-1)]
             log_likelihood = log_likelihood + np.sum(probs)
+
+            if (tags is not None) and (res_tags is not None):
+                if not tags_likelihood:
+                    tags_likelihood = 0
+
+                tags = tags[:cutoff]
+
+                tag_probs = [res_tags[n, 0, tags[n + 1].item()].item() for n in range(len(tags) - 1)]
+                tags_likelihood = tags_likelihood + np.sum(tag_probs)
 
         # perp = 2**(-(log_likelihood / num_words))
         perp = np.exp(-(log_likelihood / num_words))
-        return perp
 
+        if tags_likelihood:
+            perp_tags = np.exp(-(tags_likelihood / num_words))
+            return_value = (perp, perp_tags)
+        else:
+            return_value = perp
 
-            # for n in line:
-            #     v = n.item()
-            #
-            #     print(res[v])
-
-            # probs = [res[n.item()] for n in line]
-            # print(probs)
-            # print(np.exp(probs))
-            # print(np.product(probs))
-
+        return return_value
